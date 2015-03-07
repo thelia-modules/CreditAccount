4e3fa6bd2515f97b55d2ff99d2113cc0f87b2393
Franck Allimant <github@allimant.org>
2015-03-07 15:59:15 +0100

    Added general summary of accounts balance.

--- I18n/AdminIncludes/en_US.php
+++ I18n/AdminIncludes/en_US.php
@@ -1,13 +1,19 @@
 <?php
 
 return array(
+    'Add amount' => 'Add amount',
+    'Balance' => 'Balance',
     'Current account balance: ' => 'Current account balance: ',
+    'Current status of credit accounts' => 'Current status of credit accounts',
+    'Customer' => 'Customer',
     'Date' => 'Date',
     'Entry amount' => 'Entry amount',
     'History' => 'History',
+    'No credit account exists yet.' => 'No credit account exists yet.',
     'No entry yet in this account' => 'No entry yet in this account',
     'None' => 'None',
     'Origin' => 'Origin',
     'Related order' => 'Related order',
+    'View details' => 'View details',
     'credit account for this customer' => 'Credit account',
 );
--- I18n/AdminIncludes/fr_FR.php
+++ I18n/AdminIncludes/fr_FR.php
@@ -2,13 +2,18 @@
 
 return array(
     'Add amount' => 'Ajouter ce montant',
+    'Balance' => 'Solde',
     'Current account balance: ' => 'Solde du compte',
+    'Current status of credit accounts' => 'Solde ces comptes existants',
+    'Customer' => 'Client',
     'Date' => 'Date',
     'Entry amount' => 'Montant',
     'History' => 'Historique',
+    'No credit account exists yet.' => 'Aucune compte n\'existe actuellement.',
     'No entry yet in this account' => 'Il n\'y a aucune écriture pour l\'instant.',
     'None' => 'Aucune',
     'Origin' => 'Origine',
     'Related order' => 'Commande associée',
+    'View details' => 'Afficher les détails',
     'credit account for this customer' => 'Compte de crédit',
 );
